3ed45aa9d9d7ce81842efc5946989d91d798e38d
Nicolas Borego <129863873+NBorego@users.noreply.github.com>
2023-09-30 22:46:16 -0300

    new div

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
+const navLeft = document.getElementById('nav-left');
 const navBar = document.querySelector('nav');
 const menuButton = document.getElementById('menu-button');
 const menuClose = document.getElementById('menu-close');
 
 menuButton.addEventListener('click', () => {
     navBar.classList.replace('hidden', 'block');
+    navLeft.classList.replace('hidden', 'block');
 });
 
 menuClose.addEventListener('click', () => {
     navBar.classList.replace('block', 'hidden');
+    navLeft.classList.replace('block', 'hidden');
 });
